a0120d16c916a088b0fed36fb6f15c82b4ff1e06
Кирилл Дубов <VerkSin87@yandex.ru>
2023-04-08 16:18:53 +0300

    Добавил User

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,8 +1,13 @@
 from .permissions import IsAdminModeratorOwnerOrReadOnly
 from django.shortcuts import get_object_or_404
-from .serializers import CommentSerializer, ReviewSerializer
-from rest_framework import viewsets
-from reviews.models import Review, Title
+from .serializers import (CommentSerializer, ReviewSerializer, UserSerializer,
+                          UserEditSerializer)
+from rest_framework import permissions, status, viewsets
+from reviews.models import Review, User, Title
+from .permissions import IsAdmin, IsAdminModeratorOwnerOrReadOnly
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
@@ -33,3 +38,37 @@ class CommentViewSet(viewsets.ModelViewSet):
         review_id = self.kwargs.get('review_id')
         review = get_object_or_404(Review, id=review_id, title=title_id)
         serializer.save(author=self.request.user, review=review)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    lookup_field = "username"
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    pagination_class = PageNumberPagination
+    permission_classes = (IsAdmin,)
+
+    @action(
+        methods=[
+            "get",
+            "patch",
+        ],
+        detail=False,
+        url_path="me",
+        permission_classes=[permissions.IsAuthenticated],
+        serializer_class=UserEditSerializer,
+    )
+    def users_own_profile(self, request):
+        user = request.user
+        if request.method == "GET":
+            serializer = self.get_serializer(user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        if request.method == "PATCH":
+            serializer = self.get_serializer(
+                user,
+                data=request.data,
+                partial=True
+            )
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)

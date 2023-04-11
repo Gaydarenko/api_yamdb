04c99b84a7aafa3f7f6d99256add7121858cfbae
Евгений Гайдаренко <gaydarenko@mail.ru>
2023-04-12 00:24:38 +0300

    SystemCheckError: System check identified some issues:....

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,14 +1,14 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import (CategoryViewSet, CommentViewSet, GenreViewSet, GetToken,
-                    ReviewViewSet, SignUp, TitleViewSet, UserViewSet)
+from .views import CategoryViewSet, CommentViewSet, GenreViewSet,\
+    ReviewViewSet, TitleViewSet, UsersViewSet, api_get_token, api_signup
 
 router = routers.DefaultRouter()
 router.register(r'categories', CategoryViewSet, basename='categories')
 router.register(r'genres', GenreViewSet, basename='genres')
 router.register(r'titles', TitleViewSet, basename='titles')
-router.register(r'users', UserViewSet, basename='users')
+router.register(r'users', UsersViewSet, basename='users')
 router.register(
     r'titles/(?P<title_id>\d+)/reviews',
     ReviewViewSet,
@@ -20,6 +20,6 @@ router.register(
 
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('v1/auth/signup/', SignUp, name='signup'),
-    path('v1/auth/token/', GetToken, name='get_token'),
+    path('v1/auth/signup/', api_signup, name='signup'),
+    path('v1/auth/token/', api_get_token, name='get_token'),
 ]
